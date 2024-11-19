f8902af146aa355430bb5e1978d9e5aa4bec43cd
Dennis de Oliveira <contato@dennisoliveira.com.br>
2024-11-19 19:02:50 -0300

    Adicionando página 404 nas rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import CustomerList from './pages/CustomerList'
 import CustomerForm from './pages/CustomerForm'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<CustomerList />} />
           <Route path="/cadastro" element={<CustomerForm />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
