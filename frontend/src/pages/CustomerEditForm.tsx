26a5ea1b697a7bf418e4d3e326dcceb0a55a308e
Dennis de Oliveira <contato@dennisoliveira.com.br>
2024-11-20 00:52:57 -0300

    Adicionado campos ao form de update

--- frontend/src/pages/CustomerEditForm.tsx
+++ frontend/src/pages/CustomerEditForm.tsx
@@ -3,11 +3,29 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import { parse, isDate, format } from 'date-fns'
 import { getCustomerById, updateCustomer } from '../services/customerService'
 
 const schema = yup.object().shape({
-  Codigo: yup.string().max(150),
+  Codigo: yup.string().max(15),
   Nome: yup.string().max(150).required('Nome é obrigatório'),
+  CPF_CNPJ: yup.string().max(20),
+  CEP: yup.string().max(8),
+  Logradouro: yup.string().max(100),
+  Endereco: yup.string().max(120),
+  Numero: yup.string().max(20),
+  Bairro: yup.string().max(50),
+  Cidade: yup.string().max(60),
+  UF: yup.string().max(2),
+  Complemento: yup.string().max(150),
+  Fone: yup.string().max(15),
+  LimiteCredito: yup.number(),
+  Validade: yup.date().transform((value, originalValue) => {
+    const parsedDate = isDate(originalValue)
+      ? originalValue
+      : parse(originalValue, 'dd/MM/yyyy', new Date())
+    return parsedDate
+  }),
 })
 
 const CustomerEditForm = () => {
@@ -38,6 +56,7 @@ const CustomerEditForm = () => {
   useEffect(() => {
     ;(async () => {
       const customer = await getCustomerById(Number(id))
+      customer.Validade = format(customer.Validade, 'dd/MM/yyyy')
       reset(customer)
     })()
   }, [id, reset])
@@ -63,6 +82,115 @@ const CustomerEditForm = () => {
             {...register('Nome')}
           />
           <p>{errors.Nome?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">CPF_CNPJ:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('CPF_CNPJ')}
+          />
+          <p>{errors.CPF_CNPJ?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">CEP:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('CEP')}
+          />
+          <p>{errors.CEP?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Logradouro:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Logradouro')}
+          />
+          <p>{errors.Logradouro?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Endereco:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Endereco')}
+          />
+          <p>{errors.Endereco?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Numero:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Numero')}
+          />
+          <p>{errors.Numero?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Bairro:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Bairro')}
+          />
+          <p>{errors.Bairro?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Cidade:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Cidade')}
+          />
+          <p>{errors.Cidade?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">UF:</label>
+          <select
+            className="w-full border px-4 py-2 rounded"
+            {...register('UF')}
+          >
+            <option value="SP">SP</option>
+          </select>
+          <p>{errors.UF?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Complemento:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Complemento')}
+          />
+          <p>{errors.Complemento?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Fone:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Fone')}
+          />
+          <p>{errors.Fone?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">LimiteCredito:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('LimiteCredito')}
+          />
+          <p>{errors.LimiteCredito?.message}</p>
+        </div>
+        <div>
+          <label className="block mb-1 font-bold">Validade:</label>
+          <input
+            type="text"
+            className="w-full border px-4 py-2 rounded"
+            {...register('Validade')}
+          />
+          <p>{errors.Validade?.message}</p>
         </div>
         <br />
         <div className="flex space-x-4">
