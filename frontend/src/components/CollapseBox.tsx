9f1d2585ce3d59ee725c6d969572e7910515f428
Dennis de Oliveira <contato@dennisoliveira.com.br>
2024-11-20 17:44:18 -0300

    Criado componente para realizar o filtro dos clientes

---
+++ frontend/src/components/CollapseBox.tsx
@@ -0,0 +1,33 @@
+import React, { useState, ReactNode } from 'react'
+
+type CollapseBoxProps = {
+  title: string
+  children: ReactNode
+}
+
+const CollapseBox: React.FC<CollapseBoxProps> = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleCollapse = () => setIsOpen(!isOpen)
+
+  return (
+    <div className="w-full mx-auto my-4 shadow-md">
+      <button
+        onClick={toggleCollapse}
+        className="w-full px-4 py-2 text-left bg-gray-500 text-white font-bold flex items-center justify-between"
+      >
+        <span>{title}</span>
+        <span>{isOpen ? '-' : '+'}</span>
+      </button>
+      <div
+        className={`overflow-hidden transition-all duration-300 ${
+          isOpen ? 'max-h-screen' : 'max-h-0'
+        }`}
+      >
+        <div className="p-4 bg-white">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default CollapseBox
--- frontend/src/pages/CustomerList.tsx
+++ frontend/src/pages/CustomerList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
 import { ToastContainer, toast } from 'react-toastify'
+import CollapseBox from '../components/CollapseBox'
 import 'react-toastify/dist/ReactToastify.css'
 import {
   Customer,
@@ -37,6 +38,44 @@ const CustomerList = () => {
   return (
     <div className="container mx-auto p-4">
       <ToastContainer />
+      <CollapseBox title="Filtrar clientes">
+      <div className="flex flex-col gap-4">
+          {/* Campo Nome */}
+          <div>
+            <label
+              htmlFor="nome"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Nome
+            </label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Filtrar por nome"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+          </div>
+
+          {/* Campo Código */}
+          <div>
+            <label
+              htmlFor="codigo"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Código
+            </label>
+            <input
+              type="text"
+              id="codigo"
+              name="codigo"
+              placeholder="Filtrar por código"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+      </CollapseBox>
+      <br />
       <h1 className="text-2xl font-bold mb-4">Lista de Clientes</h1>
       <table className="table-auto w-full border-collapse border border-gray-200">
         <thead>
